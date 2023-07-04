cb354a137f0ea15a61eb395568f9c5b80488afb0
CAHill04 <115800776+CAHill04@users.noreply.github.com>
2023-07-04 16:19:36 +0100

    Name change

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Changes from Karan</Text>
+      <Text>Changes from Caoimhe</Text>
       <StatusBar style="auto" />
     </View>
   );
