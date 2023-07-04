9bed403b389933db6a5aff48cb167f0757b4417c
CAHill04 <115800776+CAHill04@users.noreply.github.com>
2023-07-04 16:33:22 +0100

    Change

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Changes from Caoimhe</Text>
+      <Text>Changes Caoimhe</Text>
       <StatusBar style="auto" />
     </View>
   );
