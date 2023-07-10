b6864a353607a5c3fe7d1afdb4dd4c540f5da4de
Karan Gupta <d22124440@mytudublin.ie>
2023-07-10 16:36:23 +0100

    firebase testing

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  FlatList,
+  SafeAreaView,
+} from "react-native";
+import app from "./firebaseConfig";
+import { useEffect, useState } from "react";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+// refer this for firestore functions: https://firebase.google.com/docs/firestore/query-data/get-data
 
 export default function App() {
+  // const [allData, setAllData] = useState([]); // this is the array of all the data from the firestore database
+
+  // const db = getFirestore(app); // this is the firestore database
+  // // console.log(db);
+
+  // // get all documents from a collection
+  // const getAllDocs = async () => {
+  //   const querySnapshot = await getDocs(collection(db, "Destinations"));
+
+  //   const data = querySnapshot.docs.map((doc) => doc.data());
+  //   console.log("Destinations", data);
+  // };
+
+  // const getAllDocs = async () => {
+  //   const querySnapshot = await getDocs(
+  //     collection(db, "Destinations", "Lisbon", "Articles")
+  //   );
+  //   // convert the data into an array of objects
+  //   const data = querySnapshot.docs.map((doc) => doc.data());
+  //   console.log("Articles", data);
+  // };
+  // const getAllDocs = async () => {
+  //   const querySnapshot = await getDocs(
+  //     collection(
+  //       db,
+  //       "Destinations",
+  //       "Lisbon",
+  //       "Articles",
+  //       "Article 1",
+  //       "Places"
+  //     )
+  //   );
+  //   // convert the data into an array of objects
+  //   const data = querySnapshot.docs.map((doc) => doc.data());
+  //   console.log("Places", data);
+  //   setAllData(data);
+  // };
+
+  // useEffect(() => {
+  //   getAllDocs();
+  // }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Something NEW</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      {/* <Button title="Get All Data" onPress={getAllDocs} />
+      <FlatList
+        data={allData}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.container}>
+              <Text>{item.placeTitle}</Text>
+              <Text>{item.placeId}</Text>
+            </View>
+          );
+        }}
+        keyExtractor={(item) => item.placeId}
+      /> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
-    fontWeight: "bold",
-    fontSize: 48,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBxu_7vriZr_HochNOZmK64QTsbm4UhyZA",
+  authDomain: "away-app-31140.firebaseapp.com",
+  projectId: "away-app-31140",
+  storageBucket: "away-app-31140.appspot.com",
+  messagingSenderId: "658063917261",
+  appId: "1:658063917261:web:69b34a8bbbf7f52ad0a984",
+};
+
+const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
+
+export default app;
