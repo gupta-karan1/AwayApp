ec8d3a2458f67cff239a848e64df3e2373e05219
Karan Gupta <d22124440@mytudublin.ie>
2023-07-11 16:02:20 +0100

    navigation tabs

---
+++ src/screens/DestinationScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const DestinationScreen = () => {
+  return (
+    <View>
+      <Text>DestinationScreen</Text>
+    </View>
+  );
+};
+
+export default DestinationScreen;
+
+const styles = StyleSheet.create({});
