ca07ff4c6c487afec49ffc67c2cd7f8deaa11f88
Karan Gupta <d22124440@mytudublin.ie>
2023-07-12 20:01:47 +0100

    removed performance settings

--- src/screens/ArticleScreen.js
+++ src/screens/ArticleScreen.js
@@ -67,12 +67,6 @@ const ArticleScreen = ({ route }) => {
               <Text>{articleUrl}</Text>
             </View>
           }
-          // Performance settings
-          removeClippedSubviews={true} // Unmount components when outside of window
-          initialNumToRender={2} // Reduce initial render amount
-          maxToRenderPerBatch={1} // Reduce number in each render batch
-          updateCellsBatchingPeriod={100} // Increase time between renders
-          windowSize={7} // Reduce the window size
         />
       )}
     </View>
