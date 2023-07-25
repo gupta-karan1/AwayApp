54b198191d7735db02c26de1b189a60661eafa3f
Karan Gupta <d22124440@mytudublin.ie>
2023-07-25 21:09:58 +0100

    minor change

--- src/screens/TripScreen/CreateTripForm.jsx
+++ src/screens/TripScreen/CreateTripForm.jsx
@@ -330,7 +330,7 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: 200,
-    backgroundColor: "lightblue",
+    backgroundColor: "lightgrey",
     borderRadius: 10,
     marginVertical: 10,
     alignItems: "center",
