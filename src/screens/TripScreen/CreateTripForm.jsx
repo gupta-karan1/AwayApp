c4df0436e42d22d55acc3e2fe4322d62a10801a8
Karan Gupta <d22124440@mytudublin.ie>
2023-09-13 17:08:04 +0100

    image

--- src/screens/TripScreen/CreateTripForm.jsx
+++ src/screens/TripScreen/CreateTripForm.jsx
@@ -811,7 +811,7 @@ const styles = StyleSheet.create({
   },
   image: {
     width: "100%",
-    height: 170,
+    height: 180,
     backgroundColor: "lightgrey",
     borderRadius: 10,
     marginVertical: 10,
